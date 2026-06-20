b5acaaf6b7ab26651b30ba7bd363bd62e0b6aa79
RafaelLanaDS <134884057+RafaelLanaDS@users.noreply.github.com>
2026-06-19 22:42:21 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/secretword/'  // ← corrige aqui!
+  base: '/secretword/'  // ← confirma que está EXATAMENTE assim
 })
